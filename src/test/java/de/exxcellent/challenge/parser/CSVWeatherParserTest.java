d060127029f80281f259e4b4ba58455435ea7721
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 12:48:24 +0200

    fix(): change order of elements in string array to pass test

--- src/test/java/de/exxcellent/challenge/parser/CSVWeatherParserTest.java
+++ src/test/java/de/exxcellent/challenge/parser/CSVWeatherParserTest.java
@@ -10,7 +10,7 @@ public class CSVWeatherParserTest {
     @Test
     public void testCSVWeatherParser() {
         CSVWeatherParser parser = new CSVWeatherParser();
-        String[] dataSet = {"1", "8", "20"};
+        String[] dataSet = {"1", "20", "8"}; //Day,MxT,MnT
         WeatherData data = parser.parse(dataSet);
 
         assertEquals(1, data.getDay());
