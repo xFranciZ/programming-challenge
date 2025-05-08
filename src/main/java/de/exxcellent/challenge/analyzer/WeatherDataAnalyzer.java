d1ecbd8edddb31420841ba29751ecc7570cca118
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 13:58:14 +0200

    feat(): implement WeatherDataAnalyzerTest

--- src/main/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzer.java
@@ -2,9 +2,11 @@ package de.exxcellent.challenge.analyzer;
 
 import de.exxcellent.challenge.model.WeatherData;
 
+import java.util.List;
+
 public class WeatherDataAnalyzer {
 
-    public WeatherData getDataWithSmallestTemperatureSpread() {
+    public WeatherData getDataWithSmallestTemperatureSpread(List<WeatherData> weatherDataList) {
         return null;
     }
 }
--- src/test/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzerTest.java
+++ src/test/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzerTest.java
@@ -1,12 +1,25 @@
 package de.exxcellent.challenge.analyzer;
 
+import de.exxcellent.challenge.model.WeatherData;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherDataAnalyzerTest {
 
     @Test
     public void testDataWithSmallestTemperatureSpread() {
+        List<WeatherData> weatherDataList = new ArrayList<>();
+        weatherDataList.add(new WeatherData(1, 5,10));
+        weatherDataList.add(new WeatherData(2, 8,20));
+        weatherDataList.add(new WeatherData(3, 12, 15));
+        weatherDataList.add(new WeatherData(4, 15,20));
 
+        WeatherDataAnalyzer weatherDataAnalyzer = new WeatherDataAnalyzer();
+        WeatherData result = weatherDataAnalyzer.getDataWithSmallestTemperatureSpread(weatherDataList);
+
+        assertEquals(3, result.getDay());
     }
 
 }
