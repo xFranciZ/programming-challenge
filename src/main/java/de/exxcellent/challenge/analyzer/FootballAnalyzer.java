54b864382759e66c340421c7970ccdc2ac5973a7
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 16:37:08 +0200

    feat() implement FootballDataAnalyzerTest

--- src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
@@ -1,4 +1,11 @@
 package de.exxcellent.challenge.analyzer;
 
+import de.exxcellent.challenge.model.FootballData;
+
+import java.util.List;
+
 public class FootballAnalyzer {
+    public FootballData getDataWithSmallestGoalDifference(List<FootballData> footballDataList) {
+        return null;
+    }
 }
--- src/test/java/de/exxcellent/challenge/analyzer/FootballDataAnalyzerTest.java
+++ src/test/java/de/exxcellent/challenge/analyzer/FootballDataAnalyzerTest.java
@@ -1,4 +1,27 @@
 package de.exxcellent.challenge.analyzer;
 
+import de.exxcellent.challenge.model.FootballData;
+import de.exxcellent.challenge.model.WeatherData;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FootballDataAnalyzerTest {
+
+    @Test
+    public void testDataWithSmallestGoalDifference() {
+        List<FootballData> footballDataList = new ArrayList<>();
+        footballDataList.add(new FootballData("Arsenal", 5, 10));
+        footballDataList.add(new FootballData("Liverpool", 10, 12));
+        footballDataList.add(new FootballData("Manchester", 12, 5));
+        footballDataList.add(new FootballData("Chelsea", 10, 3));
+
+        FootballAnalyzer footballAnalyzer = new FootballAnalyzer();
+        FootballData result = footballAnalyzer.getDataWithSmallestGoalDifference(footballDataList);
+
+        assertEquals("Liverpool", result.getTeamName());
+    }
 }
