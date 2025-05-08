04f36104c25e5acd550f7724499f13966b0ccabb
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 14:09:15 +0200

    feat(): implemented WeatherDataAnalyzer

--- src/main/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzer.java
@@ -7,6 +7,20 @@ import java.util.List;
 public class WeatherDataAnalyzer {
 
     public WeatherData getDataWithSmallestTemperatureSpread(List<WeatherData> weatherDataList) {
-        return null;
+        WeatherData result = weatherDataList.get(0);
+        int resultSpread = result.getMaxTemperature() - result.getMinTemperature();
+        int currentSpread;
+
+        for(int i = 1; i < weatherDataList.size(); i++) {
+            currentSpread = weatherDataList.get(i).getMaxTemperature() - weatherDataList.get(i).getMinTemperature();
+
+            if(currentSpread < resultSpread) {
+                resultSpread = currentSpread;
+                result = weatherDataList.get(i);
+            }
+        }
+
+        return result;
     }
 }
+
