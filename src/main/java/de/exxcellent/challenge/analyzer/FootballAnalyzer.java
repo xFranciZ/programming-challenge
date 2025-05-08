c659020a528bbc26567c25f6c0c4fdcedf5a701d
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 16:44:37 +0200

    fix(): make goal difference in FootballAnalyzer always positive

--- src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
@@ -8,13 +8,13 @@ public class FootballAnalyzer {
 
     public FootballData getDataWithSmallestGoalDifference(List<FootballData> footballDataList) {
         FootballData result = footballDataList.get(0);
-        int resultSpread = result.getGoals() - result.getAllowedGoals();
+        int resultSpread = Math.abs(result.getGoals() - result.getAllowedGoals());
         int currentSpread;
 
-        for(int i = 1; i < footballDataList.size(); i++) {
-            currentSpread = footballDataList.get(i).getGoals() - footballDataList.get(i).getAllowedGoals();
+        for (int i = 1; i < footballDataList.size(); i++) {
+            currentSpread = Math.abs(footballDataList.get(i).getGoals() - footballDataList.get(i).getAllowedGoals());
 
-            if(currentSpread < resultSpread) {
+            if (currentSpread < resultSpread) {
                 resultSpread = currentSpread;
                 result = footballDataList.get(i);
             }
