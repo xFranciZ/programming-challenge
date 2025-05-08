ec5db9a8aba514be7e47eb7ff5b452bada1dc957
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 13:50:45 +0200

    chore(): add classes WeatherDataAnayzer and WeatherDataAnayzerTest

---
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherDataAnalyzer.java
@@ -0,0 +1,10 @@
+package de.exxcellent.challenge.analyzer;
+
+import de.exxcellent.challenge.model.WeatherData;
+
+public class WeatherDataAnalyzer {
+
+    public WeatherData getDataWithSmallestTemperatureSpread() {
+        return null;
+    }
+}
