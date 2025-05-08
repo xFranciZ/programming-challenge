1df5150fa46bd3c674b17ebffc2625668301819d
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 16:42:05 +0200

    feat(): implement FootballAnalyzer

--- src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
@@ -5,7 +5,21 @@ import de.exxcellent.challenge.model.FootballData;
 import java.util.List;
 
 public class FootballAnalyzer {
+
     public FootballData getDataWithSmallestGoalDifference(List<FootballData> footballDataList) {
-        return null;
+        FootballData result = footballDataList.get(0);
+        int resultSpread = result.getGoals() - result.getAllowedGoals();
+        int currentSpread;
+
+        for(int i = 1; i < footballDataList.size(); i++) {
+            currentSpread = footballDataList.get(i).getGoals() - footballDataList.get(i).getAllowedGoals();
+
+            if(currentSpread < resultSpread) {
+                resultSpread = currentSpread;
+                result = footballDataList.get(i);
+            }
+        }
+
+        return result;
     }
 }
