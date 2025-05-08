d10486865fff8fc637a388737de546982d672e01
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 13:06:52 +0200

    feat(): implement CSVDataLoaderTest

--- src/main/java/de/exxcellent/challenge/dataloader/CSVDataLoader.java
+++ src/main/java/de/exxcellent/challenge/dataloader/CSVDataLoader.java
@@ -1,11 +1,15 @@
 package de.exxcellent.challenge.dataloader;
 
+import de.exxcellent.challenge.parser.CSVParser;
+
 import java.util.List;
 
 /**
  *  Class for loading data of a CSV file into a list of a specific type
  */
 public class CSVDataLoader<T> implements DataLoader<T>{
+
+    CSVParser<T> parser;
 
     @Override
     public List<T> load(String filePath) {
---
+++ src/test/java/de/exxcellent/challenge/dataloader/CSVDataLoaderTest.java
@@ -0,0 +1,36 @@
+package de.exxcellent.challenge.dataloader;
+
+import de.exxcellent.challenge.model.WeatherData;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVDataLoaderTest {
+
+    @Test
+    public void testCSVLoaderWithWeatherData() {
+        List<WeatherData> expectedDataList = new ArrayList<>();
+        expectedDataList.add(new WeatherData(1, 1,10));
+        expectedDataList.add(new WeatherData(2,5,11));
+        expectedDataList.add(new WeatherData(3, 2,8));
+
+
+        CSVDataLoader<WeatherData> csvWeatherDataLoader = new CSVDataLoader<>();
+        List<WeatherData> weatherDataList = csvWeatherDataLoader.load("/weather_test.csv");
+
+        assertEquals(weatherDataList.size(), expectedDataList.size());
+
+        for(int i = 0; i < weatherDataList.size(); i++) {
+            compareWeatherData(weatherDataList.get(i), expectedDataList.get(i));
+        }
+
+    }
+
+    private void compareWeatherData(WeatherData weatherData, WeatherData expectedWeatherData) {
+        assertEquals(weatherData.getDay(), expectedWeatherData.getDay());
+        assertEquals(weatherData.getMinTemperature(), expectedWeatherData.getMinTemperature());
+        assertEquals(weatherData.getMaxTemperature(), expectedWeatherData.getMaxTemperature());
+    }
+
+}
