7cbea61c08ad52135225319f309a03208ec659bd
xFranciZ <daniel.franz1999@gmail.com>
2025-05-08 12:23:59 +0200

    feat(): add CSVWeatherParserTest

--- src/test/java/de/exxcellent/challenge/parser/CSVWeatherParserTest.java
+++ src/test/java/de/exxcellent/challenge/parser/CSVWeatherParserTest.java
@@ -1,5 +1,20 @@
 package de.exxcellent.challenge.parser;
 
+import de.exxcellent.challenge.model.WeatherData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CSVWeatherParserTest {
-    
+
+    @Test
+    public void testCSVWeatherParser() {
+        CSVWeatherParser parser = new CSVWeatherParser();
+        String[] dataSet = {"1", "8", "20"};
+        WeatherData data = parser.parse(dataSet);
+
+        assertEquals(1, data.getDay());
+        assertEquals(8,data.getMinTemperature());
+        assertEquals(20, data.getMaxTemperature());
+    }
 }
